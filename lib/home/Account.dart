6eb030e1bd17173a33755f91a9e5d4454e93b8ee
Ruining Yang <Ruining_Yang@baylor.edu>
2022-01-17 02:04:53 -0500

    testing sync to apple health

--- lib/home/Account.dart
+++ lib/home/Account.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cs6234/home/Toolbar.dart';
+import 'package:health/health.dart';
 
 class Account extends StatefulWidget {
   const Account({Key? key}) : super(key: key);
@@ -9,12 +10,107 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
+  HealthFactory health = HealthFactory();
+
+  List<HealthDataPoint> _healthDataList = [];
+  num totalSteps = 0;
+  num totalDistance = 0;
+
+  ListView _contentDataReady() {
+    return ListView.builder(
+        itemCount: _healthDataList.length,
+        itemBuilder: (_, index) {
+          HealthDataPoint p = _healthDataList[index];
+          return ListTile(
+            title: Text("${p.typeString}: ${p.value}"),
+            trailing: Text('${p.unitString}'),
+            subtitle: Text('${p.dateFrom} - ${p.dateTo}'),
+          );
+        });
+  }
+
+  Future fetchData() async {
+    final types = [
+      HealthDataType.STEPS,
+      HealthDataType.DISTANCE_WALKING_RUNNING,
+    ];
+
+    final permissions = [
+      HealthDataAccess.READ,
+      HealthDataAccess.READ,
+    ];
+
+    final now = DateTime.now();
+    final yesterday = now.subtract(Duration(days: 1));
+    final beginning = DateTime(now.year, now.month, now.day);
+
+
+    bool requested = await health.requestAuthorization(types, permissions: permissions);
+
+    if (requested) {
+      try {
+        List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(beginning, now, types);
+        totalSteps = 0;
+        totalDistance = 0;
+        setState(() {
+          for(int i = 0; i < healthData.length; i++) {
+            if(healthData[i].typeString == "STEPS") {
+              totalSteps += healthData[i].value;
+            }
+            if(healthData[i].typeString == "DISTANCE_WALKING_RUNNING") {
+              totalDistance += healthData[i].value;
+            }
+          }
+        });
+
+
+        //print(healthData);
+        _healthDataList.addAll((healthData.length < 100)
+            ? healthData
+            : healthData.sublist(0, 100));
+      } catch (error) {
+        print("Exception in getHealthDataFromTypes: $error");
+      }
+
+      _healthDataList = HealthFactory.removeDuplicates(_healthDataList);
+
+    } else {
+      print("Authorization not granted");
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          ToolBar(title: "Account"),
+        children: [
+          const ToolBar(title: "Account"),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(50, 5, 50, 5),
+            child: MaterialButton(
+                //minWidth: double.infinity,
+                color: Colors.lightBlue,
+                child: const Text('Sync Data from Apple Health', style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  fetchData();
+                }
+            ),
+          ),
+          Text("Today step is: " + totalSteps.toString(), style: const TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.lightBlueAccent,
+              fontSize: 25.0,
+              fontWeight: FontWeight.w600
+          )),
+          Text("Today distance in meter is: " + totalDistance.toString(), style: const TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.lightBlueAccent,
+              fontSize: 25.0,
+              fontWeight: FontWeight.w600
+          )),
+          //Expanded(child: _contentDataReady())
+
         ]
       )
     );
--- lib/home/Activity.dart
+++ lib/home/Activity.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cs6234/home/Toolbar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class Activity extends StatefulWidget {
   const Activity({Key? key}) : super(key: key);
@@ -10,12 +11,136 @@ class Activity extends StatefulWidget {
 }
 
 class _ActivityState extends State<Activity> {
+  final firestoreInstance = FirebaseFirestore.instance;
+
+  deleteActivities(String documentId) {
+    firestoreInstance.collection("Medicines").doc(documentId).delete().then((value){
+      print("Success!");
+    });
+  }
+
+  showDetail() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("Medicines").snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if(!snapshot.hasData) {
+          return Container(
+            child: const Center(
+              child: Text("Loading", style: TextStyle(
+                  fontFamily: 'Poppins',
+                  color: Colors.blue,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500
+              ))
+            )
+          );
+        } else {
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: const AlwaysScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              DocumentSnapshot ds = (snapshot.data!).docs[index];
+              return Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 5.0),
+                  child: Slidable(
+                    endActionPane: ActionPane(
+                      motion: const ScrollMotion(),
+                      dismissible: DismissiblePane(onDismissed: () {}),
+                      children: [
+                        SlidableAction(
+                          // An action can be bigger than the others.
+                          flex: 2,
+                          onPressed: (_) {
+                          },
+                          backgroundColor: Color(0xFF21B7CA),
+                          foregroundColor: Colors.white,
+                          icon: Icons.report_problem,
+                          label: 'Modify',
+                        ),
+                        SlidableAction(
+                          // An action can be bigger than the others.
+                          flex: 2,
+                          onPressed: (_) {
+                            deleteActivities(ds.id);
+                          },
+                          backgroundColor: Colors.redAccent,
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Delete',
+                        ),
+                      ],
+                    ),
+                    child: Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(5.0, 17.0, 5.0, 0.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Expanded(
+                                      flex: 2,
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.only(left: 12.0),
+                                            child: RichText(
+                                              text: TextSpan(
+                                                  style: const TextStyle(
+                                                      fontFamily: 'Poppins',
+                                                      color: Colors.black,
+                                                      fontSize: 15.0,
+                                                      fontWeight: FontWeight.w400
+                                                  ),
+                                                  children: [
+                                                    const TextSpan(text: "Medicine: "),
+                                                    TextSpan(
+                                                        text: ds['medicine'].toString(),
+                                                        style: const TextStyle(
+                                                            fontFamily: 'Poppins',
+                                                            color: Colors.black,
+                                                            fontSize: 15.0,
+                                                            fontWeight: FontWeight.w700
+                                                        )
+                                                    )
+                                                  ]
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(height: 16.0),
+                          ],
+                        )
+                    ),
+                  )
+              );
+            },
+            itemCount: (snapshot.data!).docs.length,
+            //padding: new EdgeInsets.symmetric(vertical: 4.0) // distance from bar to the first one
+          );
+        }
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          ToolBar(title: "Activities")
+        children: [
+          const ToolBar(title: "Activities"),
+          Expanded(
+            child: showDetail()
+          )
         ]
       )
     );
